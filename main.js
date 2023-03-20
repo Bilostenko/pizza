7004274d5044ab92300c61245d61e42e510a37e7
Bilostenko <bilostenkod@gmail.com>
2023-03-20 10:42:02 +0200

    add layout

--- main.js
+++ main.js
@@ -9,8 +9,21 @@ const pepperoni = document.querySelector('#pepperoni');
 const pepper = document.querySelector('#pepper');
 const pineapple = document.querySelector('#pineapple');
 
-const dropZone = document.querySelector('.pizza__base');
+/* drag$drop */
+/* const dropZone = document.querySelector('.pizza__base');
 dropZone.ondragover = allowDrop;
 function allowDrop(event) {
   event.preventDefault();
 }
+
+cheese.ondragstart = drag;
+function drag(event) {
+  event.dataTransfer.setData('id', event.target.id);
+}
+
+dropZone.ondrop = drop;
+function drop(event) {
+   let id = event.dataTransfer.getData('id');
+  event.target.append(document.getElementById(id));
+} */
+
